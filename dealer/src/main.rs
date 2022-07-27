5f624b69cb89ee8c136198324634710ec173459e
the-kollider <info@kollider.xyz>
2022-07-27 17:20:48 +0100

    Made dealer main async

--- dealer/src/main.rs
+++ dealer/src/main.rs
@@ -5,11 +5,12 @@ use utils::xzmq::{create_pull, create_push};
 use dealer::dealer_engine::*;
 use dealer::start;
 
-fn main() {
+#[tokio::main]
+async fn main() {
     let settings = utils::config::get_config_from_env::<DealerEngineSettings>().expect("Failed to load settings.");
 
     let bank_rx = create_pull(&settings.dealer_bank_pull_address);
     let bank_tx = create_push(&settings.dealer_bank_push_address);
 
-    start(settings, bank_tx, bank_rx);
+    start(settings, bank_tx, bank_rx).await;
 }
