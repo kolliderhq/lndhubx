d458072b5aa9a01d3f82ab49e160eedcf77e1f58
the-kollider <info@kollider.xyz>
2022-07-28 12:30:42 +0100

    created insertion interval

--- bank/src/lib.rs
+++ bank/src/lib.rs
@@ -1,7 +1,8 @@
 pub mod bank_engine;
 
-use futures::prelude::*;
 use bank_engine::*;
+use futures::prelude::*;
+use std::time::Instant;
 
 use diesel::{r2d2::ConnectionManager, PgConnection};
 use zmq::Socket as ZmqSocket;
@@ -11,13 +12,12 @@ use crossbeam_channel::bounded;
 use msgs::*;
 
 use lnd_connector::connector::*;
-use rust_decimal_macros::*;
 use rust_decimal::prelude::*;
+use rust_decimal_macros::*;
 
 use influxdb2::Client;
 
 pub async fn insert_bank_state(bank: &BankEngine, client: &Client, bucket: &str) {
-
     let mut btc_balance = dec!(0);
     let mut usd_balance = dec!(0);
     let mut eur_balance = dec!(0);
@@ -34,24 +34,25 @@ pub async fn insert_bank_state(bank: &BankEngine, client: &Client, bucket: &str)
         }
     }
 
-    let points = vec![
-        influxdb2::models::DataPoint::builder("bank_states")
-            // .tag("host", "server01")
-            .field("btc_user_balance", btc_balance.to_f64().unwrap())
-            .field("eur_user_balance", eur_balance.to_f64().unwrap())
-            .field("usd_user_balance", usd_balance.to_f64().unwrap())
-            .field("fee_balance", bank.ledger.fee_account.balance.to_f64().unwrap())
-            .field("insurance_fund_balance", bank.ledger.insurance_fund_account.balance.to_f64().unwrap())
-            .field("ln_network_max_fee", bank.ln_network_max_fee.to_f64().unwrap())
-            .field("ln_network_fee_margin", bank.ln_network_fee_margin.to_f64().unwrap())
-            .field("internal_tx_fee", bank.internal_tx_fee.to_f64().unwrap())
-            .field("external_tx_fee", bank.external_tx_fee.to_f64().unwrap())
-            .field("external_tx_fee", bank.external_tx_fee.to_f64().unwrap())
-            .build().unwrap(),
-    ];
+    let points = vec![influxdb2::models::DataPoint::builder("bank_states")
+        // .tag("host", "server01")
+        .field("btc_user_balance", btc_balance.to_f64().unwrap())
+        .field("eur_user_balance", eur_balance.to_f64().unwrap())
+        .field("usd_user_balance", usd_balance.to_f64().unwrap())
+        .field("fee_balance", bank.ledger.fee_account.balance.to_f64().unwrap())
+        .field(
+            "insurance_fund_balance",
+            bank.ledger.insurance_fund_account.balance.to_f64().unwrap(),
+        )
+        .field("ln_network_max_fee", bank.ln_network_max_fee.to_f64().unwrap())
+        .field("ln_network_fee_margin", bank.ln_network_fee_margin.to_f64().unwrap())
+        .field("internal_tx_fee", bank.internal_tx_fee.to_f64().unwrap())
+        .field("external_tx_fee", bank.external_tx_fee.to_f64().unwrap())
+        .field("external_tx_fee", bank.external_tx_fee.to_f64().unwrap())
+        .build()
+        .unwrap()];
 
     client.write(bucket, stream::iter(points)).await.unwrap();
-
 }
 
 pub async fn start(
@@ -69,7 +70,11 @@ pub async fn start(
     let lnd_connector = LndConnector::new(lnd_connector_settings.clone()).await;
     let mut lnd_connector_invoices = LndConnector::new(lnd_connector_settings).await;
 
-    let influx_client = Client::new(settings.influx_host.clone(), settings.influx_org.clone(), settings.influx_token.clone());
+    let influx_client = Client::new(
+        settings.influx_host.clone(),
+        settings.influx_org.clone(),
+        settings.influx_token.clone(),
+    );
 
     let (invoice_tx, invoice_rx) = bounded(1024);
     let (priority_tx, priority_rx) = bounded(1024);
@@ -84,6 +89,8 @@ pub async fn start(
 
     let mut bank_engine = BankEngine::new(Some(pool), lnd_connector, settings.clone()).await;
     bank_engine.init_accounts();
+
+    let mut state_insertion_interval = Instant::now();
 
     insert_bank_state(&bank_engine, &influx_client, &settings.influx_bucket.clone()).await;
 
@@ -126,6 +133,9 @@ pub async fn start(
             bank_engine.process_msg(msg, &mut listener).await;
         }
 
-        insert_bank_state(&bank_engine, &influx_client, &settings.influx_bucket.clone()).await;
+        if state_insertion_interval.elapsed().as_secs() > 5 {
+            insert_bank_state(&bank_engine, &influx_client, &settings.influx_bucket.clone()).await;
+            state_insertion_interval = Instant::now();
+        }
     }
 }
