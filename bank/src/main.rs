9bd1e131a3a888f31627b92fc4e1747db63ba7cb
Konstantin Wünscher <konstantin@ip-10-66-66-19.eu-west-1.compute.internal>
2023-01-15 14:55:43 +0000

    trying to add nostr notification

--- bank/src/main.rs
+++ bank/src/main.rs
@@ -6,6 +6,14 @@ use utils::xzmq::SocketContext;
 use bank::{bank_engine::*, start};
 use lnd_connector::connector::LndConnectorSettings;
 
+use std::{
+    str::FromStr,
+    sync::{Arc, Mutex},
+    thread,
+};
+
+use nostr_rust::{nostr_client::Client, req::ReqFilter, Identity, Message, events::extract_events_ws, utils::parse_content_tags};
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let settings = utils::config::get_config_from_env::<BankEngineSettings>().expect("Failed to load settings.");
@@ -20,6 +28,18 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let dealer_rx = context.create_pull(&settings.bank_dealer_pull_address);
 
     let cli_socket = context.create_response(&settings.bank_cli_resp_address);
+
+    let my_identity =
+        Identity::from_str("6c588eb82627e659746d92a6345fee374eecfab0bc4d2b61e206e4ec5e6533b1")
+            .unwrap();
+
+    let mut nostr_client =
+        Client::new(vec!["wss://nostr-pub.wellorder.net"]).unwrap();
+
+    let pubkey = "npub1qnk4fdg4cy73x92zglazlu7ywrzxz50p4edrrjhvmvlqg9q6arrqyf6a6x";
+
+    let res = nostr_client.send_private_message(&my_identity, pubkey, "Hello from kollider!", 0);
+    dbg!(res);
 
     start(
         settings,
