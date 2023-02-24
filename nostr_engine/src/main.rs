045f93a5cd4b8c66abaa378a4e26115869b56e19
mk-kollider <105054052+mk-kollider@users.noreply.github.com>
2023-02-24 11:45:41 +0100

    Allow rebuilding nostr profile index for specified relays

--- nostr_engine/src/main.rs
+++ nostr_engine/src/main.rs
@@ -1,7 +1,7 @@
 use diesel::r2d2::{ConnectionManager, Pool};
 use diesel::PgConnection;
 use msgs::*;
-use nostr_engine::{spawn_events_handler, spawn_profile_subscriber, NostrEngineEvent, NostrEngineSettings};
+use nostr_engine::{get_relays, spawn_events_handler, spawn_profile_subscriber, NostrEngineEvent, NostrEngineSettings};
 use nostr_sdk::prelude::{FromSkStr, Keys};
 use slog as log;
 use utils::xzmq::SocketContext;
@@ -16,18 +16,8 @@ async fn main() {
     let bank_tx = context.create_push(&settings.nostr_bank_push_address);
 
     let nostr_engine_keys = Keys::from_sk_str(&settings.nostr_private_key).unwrap();
-    let relay_addresses = [
-        "wss://relay.nostr.info",
-        "wss://nostr-pub.wellorder.net",
-        "wss://relay.damus.io",
-        "wss://nostr.zebedee.cloud",
-        "wss://nostr.bitcoiner.social",
-    ];
 
-    let relays = relay_addresses
-        .iter()
-        .map(|address| (address.to_string(), None))
-        .collect::<Vec<_>>();
+    let relays = get_relays(&settings);
 
     let (events_tx, events_rx) = tokio::sync::mpsc::channel(2048);
 
@@ -35,16 +25,36 @@ async fn main() {
         .build(ConnectionManager::<PgConnection>::new(settings.psql_url))
         .expect("Failed to create pool.");
 
-    let subscribe_since = utils::time::time_now() / 1000;
+    let now_seconds = utils::time::time_now() / 1000;
+
+    // for rebuilding index
     spawn_profile_subscriber(
         nostr_engine_keys.clone(),
-        relays.clone(),
-        subscribe_since,
+        relays.subscribed_from_beginning,
+        Some(0),
+        None,
         events_tx.clone(),
         logger.clone(),
     );
 
-    spawn_events_handler(nostr_engine_keys, relays, events_rx, bank_tx, db_pool, logger.clone());
+    // for ongoing subscription
+    spawn_profile_subscriber(
+        nostr_engine_keys.clone(),
+        relays.subscribed_from_now.clone(),
+        Some(now_seconds),
+        None,
+        events_tx.clone(),
+        logger.clone(),
+    );
+
+    spawn_events_handler(
+        nostr_engine_keys,
+        relays.subscribed_from_now,
+        events_rx,
+        bank_tx,
+        db_pool,
+        logger.clone(),
+    );
 
     while let Ok(frame) = bank_recv.recv_msg(0) {
         if let Ok(message) = bincode::deserialize::<Message>(&frame) {
