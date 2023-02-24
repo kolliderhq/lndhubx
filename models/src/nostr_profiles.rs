f043c8c5b76caaf47ff368b71e6f834d6b0c72c0
mk-kollider <105054052+mk-kollider@users.noreply.github.com>
2023-02-24 18:35:33 +0100

    Upsert nostr profile record with newer only

--- models/src/nostr_profiles.rs
+++ models/src/nostr_profiles.rs
@@ -1,6 +1,20 @@
 use crate::schema::nostr_profile_records;
 use diesel::{BoolExpressionMethods, ExpressionMethods, PgTextExpressionMethods, QueryDsl, QueryResult, RunQueryDsl};
 
+fn nullable_string(value: &Option<String>) -> String {
+    match value {
+        Some(text) => format!("'{text}'"),
+        None => String::from("NULL"),
+    }
+}
+
+fn nullable_bool(value: &Option<bool>) -> String {
+    match value {
+        Some(v) => format!("{v}"),
+        None => String::from("NULL"),
+    }
+}
+
 #[derive(Queryable, Identifiable, Insertable, AsChangeset, Debug)]
 #[changeset_options(treat_none_as_null = "true")]
 #[primary_key(pubkey)]
@@ -18,12 +32,64 @@ pub struct NostrProfileRecord {
 
 impl NostrProfileRecord {
     pub fn upsert(&self, conn: &diesel::PgConnection) -> QueryResult<usize> {
-        diesel::insert_into(nostr_profile_records::dsl::nostr_profile_records)
-            .values(self)
-            .on_conflict(nostr_profile_records::dsl::pubkey)
-            .do_update()
-            .set(self)
-            .execute(conn)
+        // diesel::insert_into(nostr_profile_records::dsl::nostr_profile_records)
+        //     .values(self)
+        //     .on_conflict(nostr_profile_records::dsl::pubkey)
+        //     .do_update()
+        //     .set(self)
+        //     .filter(nostr_profile_records::dsl::created_at.lt(self.created_at))
+        //     .execute(conn)
+
+        // the above does not work because diesel does not allow specifying filtering
+        // that is where clause on the update statement after conflict
+        let pubkey = self.pubkey.clone();
+        let created_at = self.created_at;
+        let received_at = self.received_at;
+        let name = nullable_string(&self.name);
+        let display_name = nullable_string(&self.display_name);
+        let nip05 = nullable_string(&self.nip05);
+        let lud06 = nullable_string(&self.lud06);
+        let lud16 = nullable_string(&self.lud16);
+        let nip05_verified = nullable_bool(&self.nip05_verified);
+        let upsert_query = format!(
+            "\
+                INSERT INTO nostr_profile_records AS n (\
+                    pubkey, \
+                    created_at, \
+                    received_at, \
+                    name, \
+                    display_name, \
+                    nip05, \
+                    lud06, \
+                    lud16, \
+                    nip05_verified\
+                ) \
+                VALUES(\
+                    '{pubkey}', \
+                    {created_at}, \
+                    {received_at}, \
+                    {name}, \
+                    {display_name}, \
+                    {nip05}, \
+                    {lud06}, \
+                    {lud16}, \
+                    {nip05_verified}\
+                ) \
+                ON CONFLICT (pubkey) \
+                DO UPDATE \
+                SET \
+                    created_at = EXCLUDED.created_at, \
+                    received_at = EXCLUDED.received_at, \
+                    name = EXCLUDED.name, \
+                    display_name = EXCLUDED.display_name, \
+                    nip05 = EXCLUDED.nip05, \
+                    lud06 = EXCLUDED.lud06, \
+                    lud16 = EXCLUDED.lud16, \
+                    nip05_verified = EXCLUDED.nip05_verified \
+                WHERE n.created_at < EXCLUDED.created_at\
+            "
+        );
+        diesel::sql_query(upsert_query).execute(conn)
     }
 
     pub fn update_nip05_verified(
